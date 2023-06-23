161394577b56d253e66be84dc869b71c0d0e9e44
Levine Nakhone <mugenilevine@gmail.com>
2023-06-23 17:30:27 -0500

    Commiting a madlib

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# adj = input("adjective: ")
+# verb1 = input("verb: ")
+# famous_person = input("famous_person: ")
+
+# madlib= f"Eating healthy is so{adj}, it makes me so excited all the time\.Stay hydrated and {verb1}like you are {famous_person}!"
+
+# print(madlib)
+adj = input("adj :")
+verb1 = input("verb :")
+
+madlib = f"learning to code is so much {adj}, drinking coffee keeps you {verb1} all the time\n.I love coffee when coding at night."
+
+print(madlib)
+
